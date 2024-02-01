1665e0e29ab4cc3048922818a438bc506e7fb5a5
ITMASTER\MMF.Jan <mmf.jan@reative-almaviva.it>
2024-02-01 17:43:36 +0100

    first commit

---
+++ src/main/java/com/example/sample/controller/SampleController.java
@@ -0,0 +1,23 @@
+package com.example.sample.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/call")
+public class SampleController {
+
+    @GetMapping("/hello")
+    public String greetings(){
+        return "Hello worlds";
+    }
+
+    @GetMapping("/faheem")
+    public String morning(){
+        return "Good morning Faheem jan";
+    }
+
+    @PostMapping("/add/{message}")
+    public String message(@PathVariable("message") String message){
+        return "hello "+message;
+    }
+}
